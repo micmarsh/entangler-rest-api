19bda55f2a7081eae7b0def6478106f12de4d4fb
micmarsh <michael.marsh42@gmail.com>
2013-07-06 00:46:15 -0400

    added some routes, passed a get test

--- src/movienight/handler.clj
+++ src/movienight/handler.clj
@@ -3,12 +3,28 @@
     (:require [compojure.handler :as handler]
         [compojure.route :as route]))
 
+(defn- str->bool [string]
+    (if string
+        (Boolean/valueOf string)
+    ;else
+        false))
+
+(defn- handle-post [params]
+    (let [{:keys [url convert]} params]
+         (if (str->bool convert)
+                (str "converting and uploading the video " url)
+            ;else
+                (str "upload the video " url))))
+
+(def ALL_MESSAGE "you got all the videos")
+
+
 (defroutes app-routes
-    (GET "/videos" [] "you got all the videos")
-    (GET "/videos/:id" [id] (str "you got a video " id) )
-    (POST "/videos" {{url :url} :params} (str "upload the video " url))
-        (route/resources "/")
-        (route/not-found "Not Found"))
+    (GET "/videos" [] ALL_MESSAGE)
+    (GET "/videos/:id" [id] (str "you got a video " id))
+    (POST "/videos" {params :params} (handle-post params))
+    (route/resources "/")
+    (route/not-found "Not Found"))
 
 (def app
     (handler/site app-routes))
--- test/movienight/test/handler.clj
+++ test/movienight/test/handler.clj
@@ -1,14 +1,14 @@
 (ns movienight.test.handler
   (:use clojure.test
-        ring.mock.request  
+        ring.mock.request
         movienight.handler))
 
 (deftest test-app
-  (testing "main route"
-    (let [response (app (request :get "/"))]
+  (testing "gets all videos"
+    (let [response (app (request :get "/videos"))]
       (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
-  
+      (is (= (:body response) ALL_MESSAGE))))
+
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
       (is (= (:status response) 404)))))
