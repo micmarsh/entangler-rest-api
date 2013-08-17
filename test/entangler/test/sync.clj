c53d35cb5aaca3ea865ee3f87f16a178931f1a26
micmarsh <michael.marsh42@gmail.com>
2013-08-17 18:25:46 -0400

    looking good except for removing things from sets

--- test/entangler/test/sync.clj
+++ test/entangler/test/sync.clj
@@ -1,23 +1,29 @@
 (ns entangler.test.sync
     (:use
         clojure.test
-        [entangler.state :only [sockets add-socket!]]
+        [entangler.state :only [sockets add-socket! remove-socket!]]
         [marshmacros.coffee :only [cofmap]]))
 
 (def LULZ_ID "lulzlulzlulz")
 (def SOCKET {:connection "the best"})
+
 (def RANGE (range 3))
 (defn range-vector [function]
     (vec (for [i RANGE]
         (function i))))
 
+(def FAKE_SOCKETS (range-vector #(assoc SOCKET :id %)))
+(def FAKE_IDS (range-vector #(str LULZ_ID %)))
+
+
+(defn get-socket-sets [sockets]
+    (range-vector #(sockets (str LULZ_ID %))))
+
 (deftest sockets-state
     (testing "can add multiple sockets to the map socket map"
-        (let [fake-ids (range-vector #(str LULZ_ID %))
-              fake-sockets (range-vector #(assoc SOCKET :id %)) ]
-              (doseq [i RANGE]
-                (add-socket! {:_id (fake-ids i) :socket (fake-sockets i)}))
-              (is (= (count @sockets) (count RANGE)))))
+        (doseq [i RANGE]
+            (add-socket! {:_id (FAKE_IDS i) :socket (FAKE_SOCKETS i)}))
+        (is (= (count @sockets) (count RANGE))))
 
     (testing "sockets are associated to entity ID's correctly"
         (let [socket-sets (range-vector #(@sockets (str LULZ_ID %))) ]
@@ -25,6 +31,11 @@
                 (let [socket-set (socket-sets i)]
                     (is (= (count socket-set) 1))
                     (is (contains? socket-set (assoc SOCKET :id i)))))))
+
+    (testing "delete those sockets"
+        (doseq [i RANGE]
+            (remove-socket! {:_id (FAKE_IDS i) :socket (FAKE_SOCKETS i)}))
+        (is (= (count @sockets) 0)))
 
 )
 
